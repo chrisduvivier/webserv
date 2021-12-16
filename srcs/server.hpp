443bdc4834b8767348764ebd34018f5f1ee2807d
Chris Duvivier <cduvivie@student.s19.be>
2021-12-16 17:37:30 +0100

    wip. class server

--- srcs/server.hpp
+++ srcs/server.hpp
@@ -25,91 +25,91 @@
 
 class Server
 {
-    public:
-        Server() {};
-        
-        ~Server() {};
+	public:
+		Server() {};
+		
+		~Server() {};
 
-        /* handles socket creation and server address configuration, create and start listening socket */
-        int initialize(int port)
-        {
-            if ((this->_serv_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-            {
-                std::cerr << "error: Socket creation" << std::endl;
-                return (-1);
-            }
+		/* handles socket creation and server address configuration, create and start listening socket */
+		int initialize(int port)
+		{
+			if ((this->_serv_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+			{
+				std::cerr << "error: Socket creation" << std::endl;
+				return (-1);
+			}
 
-            bzero(&this->_serv_addr, sizeof(this->_serv_addr));	//zero out everything first
-            this->_serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-            this->_serv_addr.sin_port = htons(port);
-            this->_serv_addr.sin_family = AF_INET;
+			bzero(&this->_serv_addr, sizeof(this->_serv_addr));	//zero out everything first
+			this->_serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+			this->_serv_addr.sin_port = htons(port);
+			this->_serv_addr.sin_family = AF_INET;
 
-            fcntl(_serv_socket, F_SETFL, O_NONBLOCK);
+			fcntl(_serv_socket, F_SETFL, O_NONBLOCK);
 
-            if (bind(_serv_socket, (struct sockaddr *)&_serv_addr, sizeof(_serv_addr)) < 0)
-            {
-                std::cerr << "error: Socket bind" << std::endl;
-                return (-1);	
-            }
+			if (bind(_serv_socket, (struct sockaddr *)&_serv_addr, sizeof(_serv_addr)) < 0)
+			{
+				std::cerr << "error: Socket bind" << std::endl;
+				return (-1);	
+			}
 
-            if (listen(_serv_socket, BACKLOG_LEN) < 0)
-            {
-                std::cerr << "error: Socket listen" << std::endl;
-                return (-1);	
-            }
-            return (0);
-        };
+			if (listen(_serv_socket, BACKLOG_LEN) < 0)
+			{
+				std::cerr << "error: Socket listen" << std::endl;
+				return (-1);	
+			}
+			return (0);
+		};
 
-        int performSelect()
-        {
-            int			client_fd;
-            fd_set		working_sockets;
-            char		response[] = "HTTP/1.1 200 OK\r\n\r\n Congratulation !";
+		int performSelect()
+		{
+			int			client_fd;
+			fd_set		working_sockets;
+			char		response[] = "HTTP/1.1 200 OK\r\n\r\n Congratulation !";
 
-            struct timeval	time;	//for timeout
-            time.tv_sec = 1;
-            time.tv_usec = 0;
+			struct timeval	time;	//for timeout
+			time.tv_sec = 1;
+			time.tv_usec = 0;
 
-            FD_ZERO(&working_sockets);
-            while (true)
-            {
-                FD_SET(_serv_socket, &working_sockets);
-                if (select(_serv_socket + 1, &working_sockets, NULL, NULL, &time) < 0)
-                {
-                    std::cerr << "error: select error" << std::endl;
-                    return (-1);
-                }
+			FD_ZERO(&working_sockets);
+			while (true)
+			{
+				FD_SET(_serv_socket, &working_sockets);
+				if (select(_serv_socket + 1, &working_sockets, NULL, NULL, &time) < 0)
+				{
+					std::cerr << "error: select error" << std::endl;
+					return (-1);
+				}
 
-                // check if server_socket working_sockets, in other word, that is ready for reading right now.
-                if (FD_ISSET(_serv_socket, &working_sockets))
-                {
-                    if ((client_fd = accept(_serv_socket, NULL, NULL)) < 0)
-                    {
-                        std::cerr << "error: Socket accept" << std::endl;
-                        return (-1);	
-                    }
-                    
-                    // print log
-                    std::cout << "Toc toc ! Client here !" << std::endl;
-                    
-                    if (send(client_fd, response, strlen(response) , 0 ) == -1)
-                    {
-                        std::cerr << "error: Response to client" << std::endl;
-                        return -1;	
-                    }
-                    close(client_fd);
-                }
-                else {
-                    std::cout << "Waiting for connections...\n";
-                }
-                FD_CLR(_serv_socket, &working_sockets);
-            }
-            return (0);	//should never quit
-        }
-    
-    private:
-        int					_serv_socket;
-	    struct sockaddr_in	_serv_addr;
+				// check if server_socket working_sockets, in other word, that is ready for reading right now.
+				if (FD_ISSET(_serv_socket, &working_sockets))
+				{
+					if ((client_fd = accept(_serv_socket, NULL, NULL)) < 0)
+					{
+						std::cerr << "error: Socket accept" << std::endl;
+						return (-1);	
+					}
+					
+					// print log
+					std::cout << "Toc toc ! Client here !" << std::endl;
+					
+					if (send(client_fd, get_http.c_str(), strlen(get_http.c_str()) , 0 ) == -1)
+					{
+						std::cerr << "error: Response to client" << std::endl;
+						return -1;	
+					}
+					close(client_fd);
+				}
+				else {
+					std::cout << "Waiting for connections...\n";
+				}
+				FD_CLR(_serv_socket, &working_sockets);
+			}
+			return (0);	//should never quit
+		}
+	
+	private:
+		int					_serv_socket;
+		struct sockaddr_in	_serv_addr;
 };
 
 #endif
