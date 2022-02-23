dad633cb21c7ede93a2fab28d9f3f0a459cce66c
Kovlin <rlinkov@fu-c4-p3.s19.be>
2022-02-23 16:39:01 +0100

    Typo in 405| add 415.html file and default location to configFile class

--- srcs/ConfigFile.cpp
+++ srcs/ConfigFile.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ConfigFile.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldavids <ldavids@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rlinkov <rlinkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:33:18 by ldavids           #+#    #+#             */
-/*   Updated: 2022/02/16 16:41:26 by ldavids          ###   ########.fr       */
+/*   Updated: 2022/02/23 16:37:55 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -292,6 +292,7 @@ void	ConfigFile::error_page(int x)
 	_error_pages[x].insert(std::pair<int, std::string>(400, "./public_html/400.html"));
 	_error_pages[x].insert(std::pair<int, std::string>(404, "./public_html/404.html"));
 	_error_pages[x].insert(std::pair<int, std::string>(405, "./public_html/405.html"));
+	_error_pages[x].insert(std::pair<int, std::string>(415, "./public_html/415.html"));
 	_error_pages[x].insert(std::pair<int, std::string>(500, "./public_html/500.html"));
 	_error_pages[x].insert(std::pair<int, std::string>(501, "./public_html/501.html"));
 	/*std::map<int, std::string>::iterator it1 = _error_pages[x].begin();
